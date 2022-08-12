7fb05c07b4d88410b9ff51464b357380b7986459
Baeblue <seungmin941215@gmail.com>
2022-08-12 10:13:59 +0900

    remove .git files

---
+++ styling-react/src/App.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import StyledComponents from "./StyledComponents";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <StyledComponents />
+      </div>
+    );
+  }
+}
+export default App;
