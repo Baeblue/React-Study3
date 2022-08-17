4be8701bb799fcdfa775504ec802b24f72487655
Baeblue <seungmin941215@gmail.com>
2022-08-17 10:58:48 +0900

    News api를 호출하기 위한 컴포넌트 준비(styled-components 사용)

--- news-viewer/src/App.js
+++ news-viewer/src/App.js
@@ -1,32 +1,7 @@
-import { useState } from 'react';
-import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=c29dee6fb1294542b2b92f0de69e7cea',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
